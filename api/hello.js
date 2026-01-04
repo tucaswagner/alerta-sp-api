56c60140f92c8de735a045e4f421437a3c2784f6
Meu-Favoritos_IA <tucas.artes@gmail.com>
2026-01-04 19:36:02 -0300

    Add API handler for hello endpoint

--- api/hello.js
+++ api/hello.js
@@ -1,1 +1,8 @@
 
+// api/hello.js - Função separada para teste
+export default function handler(req, res) {
+  res.status(200).json({
+    message: "Hello from Vercel Function!",
+    endpoint: "/api/hello"
+  });
+}
